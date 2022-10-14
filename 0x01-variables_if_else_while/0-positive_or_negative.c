d596c5ac2d6a5bc1be60409fb5a468f488a9f98e
ruthrora <proude>
2022-10-14 16:34:06 -0700

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ int main(void)
 	int n;
 	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%D is negative\n",n);
+		printf("%D is negative\n", n);
 	}
 	return (0);
 }
