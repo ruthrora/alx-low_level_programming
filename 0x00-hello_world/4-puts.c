89af1cd69e61c54b5c2ec8e66d089fdc1ee95469
ruthrora <proude>
2022-10-14 00:53:55 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-# include < studio.h >
+#include <stdio.h>
+
 /**
- * Description: main - prints out a string
- * Return: 0 if success
+ * main - Entry point
+ * 
+ * Return: Always 0 if success
  */
-int main (void)
+int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (o);
 }
