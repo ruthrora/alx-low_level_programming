defa5f72c93a75fdcc199d015fa5f512b5b88180
ruthrora <proude>
2022-10-18 14:06:28 -0700

    prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * main - prints _putchar, followed by a new line
+ * main - prints _putchar, followed by a new line 
  * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[] = "Putchar";
+
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
-	return (0);
+
+		return (0);
 }
