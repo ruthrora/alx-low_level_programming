ae6c05a031bf1bc7b0739f9d19c68b4320a60b70
ruthrora <proude>
2022-10-19 16:06:10 -0700

    function that adds two integers and returns the result

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,22 +2,14 @@
 
 /**
  * print_last_digit - function that prints the last digit of a number
- * @i: the int to extract the last digit from
+ * @n: the int to extract the last digit from
  * Return: Value of the last digit
  */
-int print_last_digit(int i)
+int print_last_digit(int n)
 {
-	int a;
+	if (n < 0)
+		n = n - 1;
+	_putchar((n % 10) + ' 0 ');
 
-	if (i < 0)
-		i = -i;
-
-	a = i % 10;
-
-	if (a < 0)
-		a = -a;
-
-	_putchar(a + '0');
-
-	return (a);
+	return (n % 10);
 }
