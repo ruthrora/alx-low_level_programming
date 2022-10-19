77d996f31955fd5ac829be11e45c26c53cfbfc0f
ruthrora <proude>
2022-10-19 09:40:07 -0700

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - print the alphabet a to z
+ * Return: Always 0
+ */
+void print_alphabet(void);
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
