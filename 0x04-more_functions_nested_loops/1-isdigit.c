962e73d6da03eb00559d4d9445bd0687f98685e1
ruthrora <proude>
2022-10-20 01:31:29 -0700

    function that checks for a digi

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit -  function that checks for a digit (0 through 9).
+ * @a: int to be checked
+ * Return: 1 if a is a digit, 0 else
+ */
+int _isdigit(int a)
+{
+	return (a >= 'o' && a <= '9');
+}
