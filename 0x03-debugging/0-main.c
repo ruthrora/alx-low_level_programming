fabfa350d1d973909692d5ec7cf40e0f18181cfe
ruthrora <proude>
2022-10-20 14:08:47 -0700

    code that is causing the output to go into an infinite loop.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
-
 /**
  * main - Tests for a positive or negative functions
- * Return: Always 0
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negative(i)
+	positive_or_negative(i);
 
-		return (0);
+	return (0);
 }
